8bc089f31a4ed8afa928fe403115a0ff404307bf
salasvictor22 <jsalasag@unsa.edu.pe>
2024-06-22 14:44:52 -0500

    se modifico el archivo game.component.ts del componente game y se añadio la logica del juego ahorcado

--- hangman-game/src/app/game/game.component.ts
+++ hangman-game/src/app/game/game.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WordService } from '../word.service';
 
 @Component({
   selector: 'app-game',
-  standalone: true,
-  imports: [],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.css'
+  styleUrls: ['./game.component.css']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  word: string = '';
+  displayedWord: string[] = [];
+  guessedLetters: Set<string> = new Set();
+  remainingAttempts: number = 6;
+  gameOver: boolean = false;
+  gameWon: boolean = false;
 
+  constructor(private wordService: WordService) { }
+
+  ngOnInit(): void {
+    this.startGame();
+  }
+
+  startGame(): void {
+    this.word = this.wordService.getRandomWord();
+    this.displayedWord = Array(this.word.length).fill('_');
+    this.guessedLetters.clear();
+    this.remainingAttempts = 6;
+    this.gameOver = false;
+    this.gameWon = false;
+  }
+
+  guessLetter(letter: string): void {
+    if (this.guessedLetters.has(letter) || this.gameOver) {
+      return;
+    }
+
+    this.guessedLetters.add(letter);
+
+    if (this.word.includes(letter)) {
+      for (let i = 0; i < this.word.length; i++) {
+        if (this.word[i] === letter) {
+          this.displayedWord[i] = letter;
+        }
+      }
+      this.checkWin();
+    } else {
+      this.remainingAttempts--;
+      if (this.remainingAttempts === 0) {
+        this.gameOver = true;
+      }
+    }
+  }
+
+  checkWin(): void {
+    if (!this.displayedWord.includes('_')) {
+      this.gameWon = true;
+      this.gameOver = true;
+    }
+  }
 }
