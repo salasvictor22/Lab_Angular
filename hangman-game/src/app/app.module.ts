5111c55fa93d8172dd8c2e30b6c7b34093e5581e
salasvictor22 <jsalasag@unsa.edu.pe>
2024-06-22 16:37:20 -0500

    solucionando el error de enrutamiento entre la pagina  principal y la pagina del game

--- hangman-game/src/app/app.module.ts
+++ hangman-game/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 import { GameComponent } from './game/game.component';
+import { WordService } from './word-service.service';
+import { routes } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -13,9 +17,9 @@ import { GameComponent } from './game/game.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [WordService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
