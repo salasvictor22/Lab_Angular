59d18de770023d35d413973696019b89ab361fb1
salasvictor22 <jsalasag@unsa.edu.pe>
2024-06-22 12:58:21 -0500

    implementando el servcio WordService

--- hangman-game/src/app/word-service.service.ts
+++ hangman-game/src/app/word-service.service.ts
@@ -3,7 +3,13 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class WordServiceService {
+export class WordService {
+  private words = [
+    'angular', 'typescript', 'component', 'service', 'dependency', 'injection', 'directive', 'module', 'template'
+  ];
 
-  constructor() { }
+  getRandomWord() {
+    const randomIndex = Math.floor(Math.random() * this.words.length);
+    return this.words[randomIndex];
+  }
 }
